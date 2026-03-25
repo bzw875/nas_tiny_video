04b0b87ff1f6bbe2e86dfc9b259fb94ab604b547
bzw875 <bzw875@126.com>
2026-03-25 10:17:41 +0800

    Update config.js

--- config.js
+++ config.js
@@ -2,8 +2,8 @@ const NAS_CONFIG = {
   host: '192.168.1.17',
   port: 445,
   username: 'fnnas',
-  password: 'shenJ!2017',
-  targetDir: '/Volumes/banzhaowu/FormatFactory/BaiduNetdisk/',
+  password: 'abcd1234',
+  targetDir: '/Volumes/',
 }
 
 module.exports = {
